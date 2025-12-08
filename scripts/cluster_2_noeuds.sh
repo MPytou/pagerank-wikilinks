b1abe78baccbfbfd35f320d7a35702635ab78940
Maxime Poulalion <59447625+MPytou@users.noreply.github.com>
2025-12-08 13:26:15 +0100

    Update cluster_2_noeuds.sh

--- scripts/cluster_2_noeuds.sh
+++ scripts/cluster_2_noeuds.sh
@@ -3,9 +3,9 @@ CLUSTER_NAME="pr-2n"
 REGION="us-central1"
 MACHINE_TYPE="n1-standard-2" 
 DISK_SIZE="50GB"
-WORKER_COUNT=1 # 1 Master + 1 Worker
+WORKER_COUNT=2 # 1 Master + 1 Worker
 
-echo "### 1A. Démarrage Cluster 2 Nœuds (${CLUSTER_NAME}) ###"
+echo "### Démarrage Cluster 2 Nœuds (${CLUSTER_NAME}) ###"
 gcloud dataproc clusters create ${CLUSTER_NAME} \
     --region=${REGION} \
     --master-machine-type=${MACHINE_TYPE} \
@@ -16,7 +16,7 @@ gcloud dataproc clusters create ${CLUSTER_NAME} \
     --worker-boot-disk-size=${DISK_SIZE} \
     --image-version=2.1-debian11
 
-echo "### 1B. Exécution des Jobs sur ${CLUSTER_NAME} ###"
+echo "### Exécution des Jobs sur ${CLUSTER_NAME} ###"
 
 # Exécution PageRank RDD
 echo "--- Exécution PageRank RDD ---"
@@ -30,5 +30,5 @@ gcloud dataproc jobs submit pyspark ${GCS_BUCKET}/scripts/pagerank_df.py \
     --cluster=${CLUSTER_NAME} \
     --region=${REGION}
 
-echo "### 1C. Destruction du cluster ${CLUSTER_NAME} ###"
+echo "### Destruction du cluster ${CLUSTER_NAME} ###"
 gcloud dataproc clusters delete ${CLUSTER_NAME} --region=${REGION} --quiet

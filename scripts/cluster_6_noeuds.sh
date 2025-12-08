3e5d19ed558f0a9927d8cea8c20fab8148f6425c
Maxime Poulalion <59447625+MPytou@users.noreply.github.com>
2025-12-08 21:30:50 +0100

    Fix gcloud command syntax for PageRank jobs

--- scripts/cluster_6_noeuds.sh
+++ scripts/cluster_6_noeuds.sh
@@ -25,18 +25,20 @@ echo "### Exécution des Jobs sur ${CLUSTER_NAME} ###"
 
 # Exécution PageRank RDD
 echo "--- Exécution PageRank RDD ---"
-gcloud dataproc jobs submit pyspark ${GCS_BUCKET}/scripts/pagerank_rdd.py \
+gcloud dataproc jobs submit pyspark \
     --cluster=${CLUSTER_NAME} \
     --region=${REGION} \
+    "${GCS_BUCKET}/scripts/pagerank_rdd.py" \
     -- \
     --input "${DATA_INPUT_PATH}" \
     --num-parts 200
 
 # Exécution PageRank DataFrame
 echo "--- Exécution PageRank DataFrame ---"
-gcloud dataproc jobs submit pyspark ${GCS_BUCKET}/scripts/pagerank_df.py \
+gcloud dataproc jobs submit pyspark \
     --cluster=${CLUSTER_NAME} \
     --region=${REGION} \
+    "${GCS_BUCKET}/scripts/pagerank_rdd.py" \
     -- \
     --input "${DATA_INPUT_PATH}" \
     --num-parts 200

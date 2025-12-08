a8277822106c4bb4d29992755db137d919bb0c52
Maxime Poulalion <59447625+MPytou@users.noreply.github.com>
2025-12-08 20:19:11 +0100

    Enhance cluster script with job input parameters
    Added input parameters for PageRank jobs in the script.

--- scripts/cluster_6_noeuds.sh
+++ scripts/cluster_6_noeuds.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+
+# --- CONFIGURATION GLOBALE ---
+GCS_BUCKET="gs://pagerank-data"
+DATA_INPUT_PATH="${GCS_BUCKET}/data/edges_10pc.tsv"
+
 CLUSTER_NAME="pr-6n"
 REGION="us-central1"
 MACHINE_TYPE="n1-standard-2" 
 DISK_SIZE="50GB"
-PRIMARY_WORKERS=6   # 5 Workers principaux (Total 6 nœuds)
+PRIMARY_WORKERS=6  
 
 echo "### Démarrage Cluster 6 Nœuds (${CLUSTER_NAME}) ###"
 gcloud dataproc clusters create ${CLUSTER_NAME} \
@@ -22,13 +27,19 @@ echo "### Exécution des Jobs sur ${CLUSTER_NAME} ###"
 echo "--- Exécution PageRank RDD ---"
 gcloud dataproc jobs submit pyspark ${GCS_BUCKET}/scripts/pagerank_rdd.py \
     --cluster=${CLUSTER_NAME} \
-    --region=${REGION}
+    --region=${REGION} \
+    -- \
+    --input "${DATA_INPUT_PATH}" \
+    --num-parts 200
 
 # Exécution PageRank DataFrame
 echo "--- Exécution PageRank DataFrame ---"
 gcloud dataproc jobs submit pyspark ${GCS_BUCKET}/scripts/pagerank_df.py \
     --cluster=${CLUSTER_NAME} \
-    --region=${REGION}
+    --region=${REGION} \
+    -- \
+    --input "${DATA_INPUT_PATH}" \
+    --num-parts 200
 
 echo "### Destruction du cluster ${CLUSTER_NAME} ###"
 gcloud dataproc clusters delete ${CLUSTER_NAME} --region=${REGION} --quiet

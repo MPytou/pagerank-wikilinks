f461a1467b5eaae193c8b77d7862de15770857de
Maxime Poulalion <59447625+MPytou@users.noreply.github.com>
2025-12-08 20:17:34 +0100

    Add input parameters to PageRank job submissions
    Updated the script to include input parameters for PageRank jobs.

--- scripts/cluster_2_noeuds.sh
+++ scripts/cluster_2_noeuds.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# --- CONFIGURATION GLOBALE ---
+GCS_BUCKET="gs://pagerank-data"
+DATA_INPUT_PATH="${GCS_BUCKET}/data/edges_10pc.tsv"
+
 CLUSTER_NAME="pr-2n"
 REGION="us-central1"
 MACHINE_TYPE="n1-standard-2" 
@@ -22,13 +27,21 @@ echo "### Exécution des Jobs sur ${CLUSTER_NAME} ###"
 echo "--- Exécution PageRank RDD ---"
 gcloud dataproc jobs submit pyspark ${GCS_BUCKET}/scripts/pagerank_rdd.py \
     --cluster=${CLUSTER_NAME} \
-    --region=${REGION}
+    --region=${REGION} \
+    "${GCS_BUCKET}/scripts/pagerank_rdd.py" \
+    -- \
+    --input "${DATA_INPUT_PATH}" \
+    --num-parts 200
 
 # Exécution PageRank DataFrame
 echo "--- Exécution PageRank DataFrame ---"
 gcloud dataproc jobs submit pyspark ${GCS_BUCKET}/scripts/pagerank_df.py \
     --cluster=${CLUSTER_NAME} \
-    --region=${REGION}
+    --region=${REGION} \
+    "${GCS_BUCKET}/scripts/pagerank_rdd.py" \
+    -- \
+    --input "${DATA_INPUT_PATH}" \
+    --num-parts 200
 
 echo "### Destruction du cluster ${CLUSTER_NAME} ###"
 gcloud dataproc clusters delete ${CLUSTER_NAME} --region=${REGION} --quiet
